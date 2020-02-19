e5b9791a65d13febe0ec4a16360fd35e996e0aea
Yves Desjardins <yvesdesjardins@gmail.com>
2020-02-19 14:30:23 -0800

    style changes using flexbox

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,13 +5,15 @@ export default function Post(props) {
   function parsePost() {
       return (
         <div className='post'>
-          <div className='post-content'>
+          <div className='post-content-header'>
             <div className='post-content-title'>
               { props.title }
             </div>
             Posted by: { props.author }<br/>
           </div>
-          <img src={ props.url } alt='Gif' />
+          <div className='post-content'>
+            <img src={ props.url } alt='Gif' />
+          </div>
           <div className='post-content-footer'>
             <a href= { `https://www.reddit.com/${props.id}` }>Link to post</a>
           </div>
