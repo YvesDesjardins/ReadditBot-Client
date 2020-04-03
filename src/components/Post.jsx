a8dbfa19ccc49ab4b37880d69e7ae5d595ba8885
Yves Desjardins <yvesdesjardins@gmail.com>
2020-04-03 13:41:20 -0700

    update to use material ui for cards

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import '../css/Post.css';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
 
 export default function Post(props) {
   function parsePost() {
       return (
-        <div className='post'>
-          <div className='post-content-header'>
+        <Card className='post' variant='outlined'>
+        <CardHeader
+          title={ props.title }
+          subheader={ props.author }
+        />
+        <CardMedia
+          className='post-content'
+          image={ props.url }
+          title={ props.title }
+        />
+        <CardActions>
+          <Button size="small" href={ `https://www.reddit.com/${props.id}` }>
+            Open in Reddit
+          </Button>
+        </CardActions>
+          {/* <div className='post-content-header'>
             <div className='post-content-title'>
               { props.title }
             </div>
@@ -16,8 +35,8 @@ export default function Post(props) {
           </div>
           <div className='post-content-footer'>
             <a href= { `https://www.reddit.com/${props.id}` }>Link to post</a>
-          </div>
-        </div>);
+          </div> */}
+        </Card>);
     }
 
   return parsePost();
